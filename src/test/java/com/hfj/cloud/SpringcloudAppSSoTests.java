97d0211e2c77e7849dcd0935df5882cd64ab1363
Harish <Harish@Harish-PC>
2019-01-30 22:20:46 +0530

    change class name

--- src/test/java/com/hfj/cloud/SpringcloudAppSSoTests.java
+++ src/test/java/com/hfj/cloud/SpringcloudAppSSoTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringcApplicationTests {
+public class SpringcloudAppSSoTests {
 
 	@Test
 	public void contextLoads() {
